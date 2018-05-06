1b212bf2c6bfee09d7a2bf1df51c158179bf7168
Cender <CenderG@win.dowjones.net>
2018-05-06 16:57:51 -0400

    added bootstrap formating

--- config/connection.js
+++ config/connection.js
@@ -4,23 +4,26 @@
 require("dotenv").config();
 
 var db_connect_info = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  host: process.env.DB_HOST || "localhost" ,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "mildew20",
+  database: process.env.DB_DATABASE || "burger_db"
 };
 
 var mysql = require("mysql");
 
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "mildew20",
-  database: "burger_db"
-});
+var connection = mysql.createConnection( db_connect_info );
+  
+//   {
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "mildew20",
+//   database: "burger_db"
+// }
+
 
 // Make connection.
 connection.connect(function(err) {
